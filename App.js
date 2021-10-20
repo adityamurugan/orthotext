83a66b961abcffd42915f1f0ea320d0afe099cf1
adityamurugan <48570983+adityamurugan@users.noreply.github.com>
2021-10-20 11:52:54 -0400

    made buttons bigger

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import * as Device from 'expo-device';
 import React, { useState, useEffect } from 'react';
 import { StyleSheet, TouchableOpacity, View, Button, Text, TextPropTypes } from 'react-native';
 import { ExpButton } from './components/expButton';
+import { TapResult } from './components/tapResultMap';
 import { resultPage } from './components/resultPage';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
@@ -11,12 +12,12 @@ const db = SQLite.openDatabase("result.db")
 
 //Home screen function
 const BeginPage = ({navigation}) => {
-  return <View style={styles.container}>
-          <TouchableOpacity style={styles.homeButton} onPress = {() => navigation.navigate('LandingPage', {testSelected: 'Tapping'})}>
-            <Text>Tapping Test</Text>
+  return <View style={styles.homeContainer}>
+          <TouchableOpacity style={styles.welcomeButton} onPress = {() => navigation.navigate('LandingPage', {testSelected: 'Tapping'})}>
+            <Text style={{textAlign:"left", fontSize:25, fontWeight:"bold"}}>Tapping Test</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={{...styles.homeButton}} onPress = {() => navigation.navigate('LandingPage', {testSelected: 'Swipe'})}>
-            <Text>Swipe Test</Text>
+          <TouchableOpacity style={{...styles.welcomeButton}} onPress = {() => navigation.navigate('LandingPage', {testSelected: 'Swipe'})}>
+            <Text style={{textAlign:"left", fontSize:25, fontWeight:"bold"}}>Swiping Test</Text>
           </TouchableOpacity>
         </View> 
 }
@@ -26,13 +27,6 @@ const LandingPage = ({route, navigation}) => {
   const { testSelected } = route.params;
   const [startTime, setStartTime] = useState("Begin")
   useEffect(() => {
-    /*db.transaction((tx) => {
-      tx.executeSql("insert into summary (device, testProduct) values (?, ?)", ['test','test'],
-      (t, success) => { console.log(success) });
-      tx.executeSql("select * from summary", [], (_, { rows }) =>
-      console.log(JSON.stringify(rows))
-    );
-    });*/
       if(startTime>0){
           const toggle = setInterval(() => {
               setStartTime(startTime => startTime-1);
@@ -55,6 +49,7 @@ const LandingPage = ({route, navigation}) => {
 }
 
 const Stack = createNativeStackNavigator();
+
 export default function App() {
   useEffect(() => {
     db.transaction((tx) => {
@@ -81,7 +76,8 @@ export default function App() {
           <Stack.Screen name="Home" component={BeginPage}  options={{title: 'Select a test'}}/>
           <Stack.Screen name="LandingPage" component={LandingPage} options={({ route }) => ({ title: route.params.testSelected + " test" })}/>
           <Stack.Screen name="TappingScreen" component={ExpButton}  options={{title: 'Tapping test'}}/>
-          <Stack.Screen name="resultPage" component={resultPage}  options={{title: 'Result Summary'}}/>
+          <Stack.Screen name="resultPage" component={resultPage}  options={{title: 'Results'}}/>
+          <Stack.Screen name="TapResult" component={TapResult}  options={{title: 'Detailed Result' }}/>
         </Stack.Navigator>
       </NavigationContainer>
   );
@@ -102,22 +98,23 @@ const styles = StyleSheet.create({
     backgroundColor: "#DDDDDD",
     padding: 20,
     borderRadius: 20,
-  }
+  },
+  welcomeButton: {
+    margin: 20,
+    elevation: 10,
+    borderWidth: 0.5,
+    justifyContent: "flex-end",
+    width: 150,
+    height:150,
+    backgroundColor: "#DDDDDD",
+    padding: 20,
+    borderRadius: 10,
+  },
+  homeContainer: {
+    flex: 1,
+    backgroundColor: '#fff',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 });
-
-
-    /*<View style={styles.container}>
-      <StatusBar style="auto" />
-      {state === 'home' && (
-      <BeginButton ButtonPress = {() => setState('timer')}/>
-      )}
-      {state === 'timer' && (
-      <Countdown endTimer = {() => setState('start')}/>
-      )}
-      {state === 'start' && (
-      <React.Fragment>
-        <ExpButton endExp = {() => setState('home')}/>
-        <ResetButton ButtonPress = {() => setState('home')}/>
-      </React.Fragment>
-      )}
-    </View>*/
