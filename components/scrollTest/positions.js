0c1f7441bebd0fd00195333c9c48cc14e7b6b7dc
adityamurugan <48570983+adityamurugan@users.noreply.github.com>
2021-11-04 17:39:54 -0400

    new scroll test pages

---
+++ components/scrollTest/positions.js
@@ -0,0 +1,204 @@
+import {Dimensions} from 'react-native'
+
+exports.positionsData = [
+    {
+      "alignment": 90,
+      "bottom": 20,
+      "left": 40
+    },
+    {
+      "alignment": 90,
+      "bottom": 20,
+      "left": 20
+    },
+    {
+      "alignment": 90,
+      "bottom": 20,
+      "left": 0
+    },
+    {
+      "alignment": 90,
+      "bottom": 20,
+      "left": -20
+    },
+    {
+      "alignment": 90,
+      "bottom": 20,
+      "left": -40
+    },
+    {
+      "alignment": 90,
+      "bottom": 70,
+      "left": 40
+    },
+    {
+      "alignment": 90,
+      "bottom": 70,
+      "left": 20
+    },
+    {
+      "alignment": 90,
+      "bottom": 70,
+      "left": 0
+    },
+    {
+      "alignment": 90,
+      "bottom": 70,
+      "left": -20
+    },
+    {
+      "alignment": 90,
+      "bottom": 70,
+      "left": -40
+    },
+    {
+      "alignment": 270,
+      "bottom": 20,
+      "left": 40
+    },
+    {
+      "alignment": 270,
+      "bottom": 20,
+      "left": 20
+    },
+    {
+      "alignment": 270,
+      "bottom": 20,
+      "left": 0
+    },
+    {
+      "alignment": 270,
+      "bottom": 20,
+      "left": -20
+    },
+    {
+      "alignment": 270,
+      "bottom": 20,
+      "left": -40
+    },
+    {
+      "alignment": 270,
+      "bottom": 70,
+      "left": 40
+    },
+    {
+      "alignment": 270,
+      "bottom": 70,
+      "left": 20
+    },
+    {
+      "alignment": 270,
+      "bottom": 70,
+      "left": 0
+    },
+    {
+      "alignment": 270,
+      "bottom": 70,
+      "left": -20
+    },
+    {
+      "alignment": 270,
+      "bottom": 70,
+      "left": -40
+    },
+    {
+      "alignment": 0,
+      "bottom": 0,
+      "left": 0
+    },
+    {
+      "alignment": 0,
+      "bottom": 10,
+      "left": 0
+    },
+    {
+      "alignment": 0,
+      "bottom": 20,
+      "left": 0
+    },
+    {
+      "alignment": 0,
+      "bottom": 30,
+      "left": 0
+    },
+    {
+      "alignment": 0,
+      "bottom": 40,
+      "left": 0
+    },
+    {
+      "alignment": 0,
+      "bottom": 50,
+      "left": 0
+    },
+    {
+      "alignment": 0,
+      "bottom": 60,
+      "left": 0
+    },
+    {
+      "alignment": 0,
+      "bottom": 70,
+      "left": 0
+    },
+    {
+      "alignment": 0,
+      "bottom": 80,
+      "left": 0
+    },
+    {
+      "alignment": 0,
+      "bottom": 90,
+      "left": 0
+    },
+    {
+      "alignment": 180,
+      "bottom": 0,
+      "left": 0
+    },
+    {
+      "alignment": 180,
+      "bottom": 10,
+      "left": 0
+    },
+    {
+      "alignment": 180,
+      "bottom": 20,
+      "left": 0
+    },
+    {
+      "alignment": 180,
+      "bottom": 30,
+      "left": 0
+    },
+    {
+      "alignment": 180,
+      "bottom": 40,
+      "left": 0
+    },
+    {
+      "alignment": 180,
+      "bottom": 50,
+      "left": 0
+    },
+    {
+      "alignment": 180,
+      "bottom": 60,
+      "left": 0
+    },
+    {
+      "alignment": 180,
+      "bottom": 70,
+      "left": 0
+    },
+    {
+      "alignment": 180,
+      "bottom": 80,
+      "left": 0
+    },
+    {
+      "alignment": 180,
+      "bottom": 90,
+      "left": 0
+    }
+   ]
